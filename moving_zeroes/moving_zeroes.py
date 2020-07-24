f3f3100385195a66c892ce0d17744fd2714f1606
Coding Duck <57272996+CodingDuckmx@users.noreply.github.com>
2020-07-23 19:29:26 -0500

    improvements in the solutions in terms of time and space complexity

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -2,22 +2,60 @@
 Input: a List of integers
 Returns: a List of integers
 '''
+# def moving_zeroes(arr):
+# #     # Your code here
+
+# #     # Create a new list with non zero values, and count the values of the original array.
+
+#     lst = []
+#     count = 0
+
+#     for i in range(0,len(arr)):
+#         if arr[i] != 0:
+#             lst.append(arr[i])
+#         else:
+#             count += 1
+
+#     return lst + [0] * count
+
+
+###### Improved version ######
+
 def moving_zeroes(arr):
-    # Your code here
 
-    # Create a new list with non zero values, and count the values of the original array.
+    left = 0
+    right = len(arr) -1 
 
-    lst = []
-    count =0
+    while left < right:
 
-    for i in range(0,len(arr)):
-        if arr[i] !=0:
-            lst.append(arr[i])
-        else:
-            count +=1
+        # if arr[left] == 0 and arr[right] != 0:
+        #     arr[left], arr[right] = arr[right], arr[left]
+        #     left += 1
+        #     right -= 1
 
-    return lst + [0] * count
+        # elif arr[left] !=0 and arr[right] == 0:
+        #     left +=1
 
+        # elif arr[left] == 0 and arr[right] == 0:
+        #     right -=1
+        # else:
+        #     right -=1
+
+        #### or ####
+
+        if arr[left] == 0 and arr[right] != 0:
+            arr[left], arr[right] = arr[right], arr[left]
+            left += 1
+            right -= 1
+
+        if arr[left] !=0:
+            left +=1
+
+        if arr[right] == 0:
+            right -=1
+
+
+    return arr
 
 if __name__ == '__main__':
     # Use the main function here to test out your implementation
